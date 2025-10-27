a847949dbfd3bebca5a1a379c223d4665a6bfed9
Praiz001 <oshilimpraisek@gmail.com>
2025-10-27 09:29:06 +0100

    fix: setup knex prod config

--- knexfile.ts
+++ knexfile.ts
@@ -25,6 +25,27 @@ const config: { [key: string]: Knex.Config } = {
         seeds: {
             directory: "./src/db/seeds",
         }
+    },
+    production: {
+        client: "mysql2",
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            ssl: process.env.DB_SSL_CA ? {
+                    ca: Buffer.from(process.env.DB_SSL_CA, 'base64'),
+                    rejectUnauthorized: false
+                }
+                : false
+        },
+        migrations: {
+            directory: "./src/db/migrations",
+        },
+        seeds: {
+            directory: "./src/db/seeds",
+        }
     }
 };
 
